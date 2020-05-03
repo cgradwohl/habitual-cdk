87d01bcbd7c8e8f58d4cd7bd2cd1a0a51f33a2c0
Chris Gradwohl <christophergradwohl@gmail.com>
2020-05-02 21:38:58 -0700

    add WidgetService.

--- lib/habitual-cdk-stack.ts
+++ lib/habitual-cdk-stack.ts
@@ -1,13 +1,13 @@
 import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3';
 
+import { WidgetService } from './WidgetService';
+
 export class HabitualCdkStack extends cdk.Stack {
 
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, 'HabitualTest', {
-      versioned: true
-    });
+    new WidgetService(this, 'Widgets');
   }
 }
